b57b79de61f68ea1ecc32e8d614e240d265e23c6
Johan T. Halseth <johanth@gmail.com>
2018-09-10 13:16:13 +0200

    neutrino: make UtxoScanner use BestReadyBlock instead of BestSnapshot
    This fixes an issue where we would try to scan for an utxo in a block
    where we didn't have the filter header yet, and fetching filters would
    fail, failing the scan.

--- neutrino.go
+++ neutrino.go
@@ -732,7 +732,7 @@ func NewChainService(cfg Config) (*ChainService, error) {
 	}
 
 	s.utxoScanner = NewUtxoScanner(&UtxoScannerConfig{
-		BestSnapshot:       s.BestSnapshot,
+		BestSnapshot:       s.BestReadyBlock,
 		GetBlockHash:       s.GetBlockHash,
 		BlockFilterMatches: s.blockFilterMatches,
 		GetBlock:           s.GetBlock,
